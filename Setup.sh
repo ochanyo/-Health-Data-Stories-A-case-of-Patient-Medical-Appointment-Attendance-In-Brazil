7d3a54fe6992ab65c3c984f870aa4fb8b5e8cf49
Your Name <aochanyo@gmail.com>
2022-06-22 19:43:25 +0300

    Deployment of App to Heroku

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,2 @@
-mkdir -p ~/.streamlit/ 
-     echo "\ [server]\n\
-     headless = true\n\
-     port = $PORT\n\
-     enableCORS = false\n\
-     \n\" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/ echo "\ [server]\n\headless = true\n\port = $PORT \n\enableCORS = false \n\\n\"
+> ~/.streamlit/config.toml
